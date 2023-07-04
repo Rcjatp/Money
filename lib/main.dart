df39379fe1e9733e60d1435dd79dd3d90f7390aa
rcjtp <rcjtp08@gmail.com>
2023-07-04 09:23:44 +0700

    moneyapp

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:moneymanager/pages/splash.dart';
+import 'package:moneymanager/theme.dart';
+
+
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('money');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expenses',
+      theme: CustomTheme.myTheme,
+      home: const Splash(),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static const MaterialColor primaryMaterialColor = MaterialColor(
+    4279567515,
+    <int, Color>{
+      50: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .1,
+      ),
+      100: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .2,
+      ),
+      200: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .3,
+      ),
+      300: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .4,
+      ),
+      400: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .5,
+      ),
+      500: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .6,
+      ),
+      600: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .7,
+      ),
+      700: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .8,
+      ),
+      800: Color.fromRGBO(
+        21,
+        4,
+        155,
+        .9,
+      ),
+      900: Color.fromRGBO(
+        21,
+        4,
+        155,
+        1,
+      ),
+    },
+  );
+
+  static final ThemeData myTheme = ThemeData(
+    fontFamily: "customFont",
+    primaryColor: Color(0xff15049b),
+    primarySwatch: primaryMaterialColor,
+    appBarTheme: AppBarTheme(
+      titleTextStyle: TextStyle(
+        fontSize: 20.0,
+        fontFamily: "customFont",
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+  );
+}
